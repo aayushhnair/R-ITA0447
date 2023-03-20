d68e4e1466a6cb455b762dfcebdcc99552bb9b76
Ayush V Nair <96865576+xakvn@users.noreply.github.com>
2023-03-20 21:59:33 +0530

    Create Program_2.r

---
+++ Program_2.r
@@ -0,0 +1,7 @@
+Program 2 
+
+Part A & B
+
+gender <- c("female", "male", "male", "female", "male", "female")
+gender_factor <- factor(gender, levels = c("female", "male"))
+is.factor(gender_factor)
